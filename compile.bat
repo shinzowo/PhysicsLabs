e02bf2cce4dea74b57d775994a6db33af1fcc393
dok23 <dok23@tpu.ru>
2024-12-08 20:39:24 +0700

    Fix compile.bat.

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
-.\.env\Scripts\pyinstaller.exe -F main.py --optimize 2 -w --disable-windowed-traceback --workpath "./.build/build" --specpath "./.build" --distpath "./.build/dist" --icon "../icons/icon.png"
+pyinstaller -F main.py --optimize 2 -w --disable-windowed-traceback --workpath "./.build/build" --specpath "./.build" --distpath "./.build/dist" --icon "../icons/icon.png"
 xcopy /e /k /h /i /y .\otchot .\.build\dist\otchot
 xcopy /e /k /h /i /y .\data .\.build\dist\data
 xcopy /e /k /h /i /y .\images .\.build\dist\images
